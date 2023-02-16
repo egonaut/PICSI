34e36f5365c7c177cf24beebe62307411c224b14
waterfalls <help@blackmesa.id>
2023-02-16 13:56:20 -0500

    Update PICSI.bat

--- PICSI.bat
+++ PICSI.bat
@@ -1,23 +1,24 @@
 @echo off&color a && Title [PICSI - PNF/INF/CAT/SYS Installer ]
 echo Welcome to PICSI, the universal PNF/INF/CAT/SYS Driver Installation Tool!
+echo https://github.com/egonaut/PICSI/
 
 :: Overly Convoluted Way of Adding a Delay LOL
 ping -n 4 localhost 1>nul
 
-:: Run from CD
+:: Run from CD if necessary
 cd %~dp0
 
 :: Scan and Echo Files During Install
 for /f "tokens=* delims=" %%a in ('dir /b /s /o:gen *.inf') do (
 echo == Installing == "%%a"
 
-:: Overly Convoluted Way of Adding a Delay LOL
+:: Overly Convoluted Way of Adding a Delay Again Because I'm Just Insane
 ping -n 4 localhost 1>nul
 
 :: Windows Plug-n-Play Installer
 start "" pnputil -i -a %%a
 )
-echo * DONE *
+echo * ALL DONE! *
 Echo.&Echo.&Echo.
-echo == Reboot to finalize driver installs! ==&pause>nul
+echo == Reboot to finalize all driver installations :) ==&pause>nul
 exit
